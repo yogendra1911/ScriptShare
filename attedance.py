839223b9112b4a695ed6ffe5f9410ed7eab494af
Yogendra <yogendra1911@gmail.com>
2020-12-18 13:55:33 +0530

    v0.5
    - Auto date extraction from file
    - Removed the requirement of renaming the attendance file

--- attedance.py
+++ attedance.py
@@ -1,70 +1,66 @@
 ############################################################
 # Create an attedance list with MS teams meeting attendance#
-# csv files. The files needs to be renamed to have date as #
-# last part of the filename - ex. "att - aug 05.csv"       #
-# Redirect output to a file to store the result and rename #
-# as CSV                                                   #
+# csv files.                                               #
 # V0.2 - muster generation                                 #
 # V0.3 - bugfix duplicate entry of faculty                 #
 # V0.4 - percentage attendace of students                  #
 #        auto musterfile generation                        #
+# V0.5 - Date extraction from file, no need to rename the  # 
+#        original attendance files                         #
 # Run as:                                                  #
 # `$ python attendance.py`                                 #
 # Author - Yogendra Tank                                   #
 ############################################################
 
-import os 
-def removeEmpty(l):
-    for i in range(len(l)):
-        l[i]=l[i].strip()
-    return list(filter(None,l))
-
-def extract_dates():
-    dates = []
-    for file in os.listdir("./"):
-        if file.endswith(".csv"):
-            dates.append(" ".join(file.split(".")[0].split(" ")[-2:]))
-    return dates         
-
-dt = ''
+import os
+from datetime import datetime
+# def removeEmpty(l):
+#     for i in range(len(l)):
+#         l[i]=l[i].strip()
+#     return list(filter(None,l))
+      
 students = {}
 attendance2 = {}
-dates = extract_dates()
+datesMaster = []
 
 for file in os.listdir("./"):
     att_for_date = []
     att = {}
     att_date = ''
     if file.endswith(".csv"):
-        dt = " ".join(file.split(".")[0].split(" ")[-2:])
-        f = open(file)
+        f = open(file,encoding="utf-16")
         lines = f.readlines()
-        lines = removeEmpty(lines)
+        #lines = removeEmpty(lines)
         enroll = []
         for i in range(3,len(lines)):
                 if lines[i] == '\x00': continue
                 if "-" not in lines[i]: continue #escape duplicate entry for faculty name
+                l = str(lines[i].split("\t")[2].strip('"').strip()) ## Read the datetime from the line
+                tmp = datetime.strptime(l.split("\t")[-1],'%m/%d/%Y, %H:%M:%S %p') 
+                tempDate = tmp.strftime("%d %b %Y")
+                if tempDate not in datesMaster:
+                    datesMaster.append(tempDate)
                 a = lines[i].split("\t")[0].split("-")
                 name = a[-1]
                 enroll = a[-2]
                 students[enroll] = name
-                att[enroll] = dt  
-                att_date = dt
+                att[enroll] = tempDate  
+                att_date = tempDate	
+        f.close()
     for k in att:
         att_for_date.append(k) 
     attendance2[att_date] = att_for_date
 
 f = open("./temp","w")     
 f.write("Enrollment Number , Student Name,")
-for date in dates:
+for date in datesMaster:
     f.write(date+",")
 f.write("% \n")
-#print (len(date))
 for studs in students:
     p=0
     dt = 0
     f.write (str(studs)+","+str(students[studs])+",")
-    for date in dates:
+    for date in datesMaster:
         dt = dt+1
         if studs in attendance2[date]:
             f.write("P,")
@@ -73,4 +69,4 @@ for studs in students:
             f.write("A,")
     f.write(str(round((p*100)/dt,2))+"\n")
 f.close()
-os.rename("./temp","AttMuster.csv")
+os.rename("./temp","Att-Muster.csv")
